ddb2f68511688ce9df54d6329c054cb70a1ed0c7
MavericCod <921chavanprem@gmail.com>
2024-05-29 21:42:11 +0530

    completed D2- Class Work Assignment

---
+++ D2_Classwork/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    
+    <div>
+      <h1>Learn Web Developement</h1>
+      <div>
+        <p>Welcome to the MDN Learning area, This set of articles aims to complete beginners to web developement with all that they need to start cooding websites.</p>
+      </div>
+      <div>
+        <p>The aim of this area of MDN is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfirtable." From there, you shoul dbe able to start making your way, learning from <a href="">the rest of MDN,</a> and other intermediate to advanced resources that assume a lot of previous knowledge.</p>
+      </div>
+      <div>
+        <p>If you are a complete begineer, web developememt can be challenging == we will hold your hand and provide enough details for you to feel comfirtable and learn the topics properly. you should feel at home wheather you are a student learning web developement (on your own or as part of a class ), a teacher looking for class materials, a hobbyiest, or someone who just wants to understand more about how web technologies work.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
